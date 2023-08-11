392f4100c620aadb9b717cc33e9e22a91efaaa15
Kate <mitflash56@gmail.com>
2023-08-11 18:24:12 +0300

    changed logo

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -4,9 +4,7 @@ import HeroSvg from "./components/herosvg";
 import React from 'react'
 
 export default async function Home() {
-  const profile: ProfileType[] = await getProfile();
-  console.log("🚀 ~ file: page.tsx:8 ~ Home ~ profile:", profile)
-  
+  const profile: ProfileType[] = await getProfile();  
   return (
     <main className="max-w-7xl mx-auto lg:px-16 px-6">
       <section className="flex xl:flex-row flex-col xl:items-center items-start xl:justify-center justify-between gap-x-12 lg:mt-32 mt-20 mb-16">
